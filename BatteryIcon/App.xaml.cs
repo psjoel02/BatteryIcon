fcbe11736107fcf287d379a64e141596ca1c8e50
Joel Pimenta <psjoel02@gmail.com>
2022-07-12 00:28:24 -0500

    - Changed ContextMenuStrip to regular ContextMenu - MainWindow now updates with NotifyIcon To-Do: - Add power plan slider and brightness to window - Battery Status and Settings

--- BatteryIcon/App.xaml.cs
+++ BatteryIcon/App.xaml.cs
@@ -7,7 +7,7 @@ using Forms = System.Windows.Forms;
 using Windows.Devices.Power;
 using Windows.UI.Core;
 using Microsoft.Win32;
-
+using System.Windows.Media.Imaging;
 
 namespace BatteryIcon
 {
@@ -17,9 +17,14 @@ namespace BatteryIcon
         private static bool open = false;
         private static bool notified = false;
         bool reportRequested = false;
+
         protected static Forms.NotifyIcon _notifyIcon = new Forms.NotifyIcon();
         private static Forms.PowerStatus _pwr = Forms.SystemInformation.PowerStatus;
+
         private MainWindow form = new MainWindow();
+        public MainWindow access = new MainWindow();
+        private Settings s = new Settings();
+
         public App()
         {
             Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
@@ -34,18 +39,14 @@ namespace BatteryIcon
 
             _notifyIcon.MouseClick += notifyIcon_MouseClick;
 
-            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
-            _notifyIcon.ContextMenuStrip.Items.Add("Status", Image.FromFile("Resources\\charging.ico"), OnStatusClicked);
-            _notifyIcon.ContextMenuStrip.Items.Add("Test", null, OnTestClicked);
-            _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, OnExitClicked);
+            _notifyIcon.ContextMenu = new Forms.ContextMenu();
+            _notifyIcon.ContextMenu.MenuItems.Add(new Forms.MenuItem("Power", OnPowerClicked));
+            _notifyIcon.ContextMenu.MenuItems.Add(new Forms.MenuItem("Settings", OnSettingsClicked));
+            _notifyIcon.ContextMenu.MenuItems.Add(new Forms.MenuItem("Battery Status", OnSettingsClicked));
+            _notifyIcon.ContextMenu.MenuItems.Add(new Forms.MenuItem("Exit", OnExitClicked));
             _notifyIcon.Visible = true;
 
             base.OnStartup(e);
-
-        }
-
-        private void OnStatusClicked(object sender, EventArgs e)
-        {
 
         }
 
@@ -57,14 +58,36 @@ namespace BatteryIcon
             Environment.Exit(0);
         }
 
-        private void OnTestClicked(object sender, EventArgs e)
+        private void OnSettingsClicked(object sender, EventArgs e)
         {
-            //test functions here
+            //display Github page, release notes, license and check for updates
+            //allow user to change between number and percentage icon, autostart with windows, and dark/light theme
+            //along with Battery Icon (name), Taskbar icon, version number, and author 2022
+            //user should be able to check for updates and uninstall application
+            s.Left = SystemParameters.WorkArea.Width - 350 - 2;
+            s.Top = SystemParameters.WorkArea.Height - 250 - 2;
+            //form.SetDesktopLocation(MousePosition.X - form.Width / 2, MousePosition.Y - form.Height - 20);
+            s.Show();
+            s.Activate();
+        }
+
+        private void OnStatusClicked(object sender, EventArgs e)
+        {
+            //battery type, wear level, power state, designed and fully charged capacity
+            //current capacity, battery voltage, charge/discharge rate, current power source, 
+            //battery charge, and remaining battery lifetime
+            SetIcon(_pwr);
+            SetText(_pwr);
+        }
+
+        private void OnPowerClicked(object sender, EventArgs e)
+        {
+            var cplPath = System.IO.Path.Combine(Environment.SystemDirectory, "control.exe");
+            System.Diagnostics.Process.Start(cplPath, "/name Microsoft.PowerOptions");
         }
 
         private void notifyIcon_MouseClick(object sender, Forms.MouseEventArgs e)
         {
-
 
             Forms.MouseEventArgs mouseEventArgs = (Forms.MouseEventArgs)e;
             if (mouseEventArgs.Button == Forms.MouseButtons.Left)
@@ -92,7 +115,7 @@ namespace BatteryIcon
         }
 
 
-        public static void SetIcon(Forms.PowerStatus _pwr)
+        public void SetIcon(Forms.PowerStatus _pwr)
         {
 
             Double BatteryLifeRemaining = Math.Round(_pwr.BatteryLifeRemaining / 60.0);
@@ -101,23 +124,29 @@ namespace BatteryIcon
             if (_pwr.PowerLineStatus == Forms.PowerLineStatus.Unknown)
             {
                 _notifyIcon.Icon = new Icon("Resources\\no_battery.ico");
+                form.SetIcon("Resources\\no_battery.ico");
             }
             else if (_pwr.PowerLineStatus == Forms.PowerLineStatus.Online)
             {
                 _notifyIcon.Icon = new Icon("Resources\\charging.ico");
+                form.SetIcon("Resources\\charging.ico");
+                
             }
             else if (Batterylife >= 80 && Batterylife <= 100)
             {
                 _notifyIcon.Icon = new Icon("Resources\\high.ico");
+                form.SetIcon("Resources\\high.ico");
             }
             else if (Batterylife >= 50 && Batterylife <= 79)
             {
                 _notifyIcon.Icon = new Icon("Resources\\mid.ico");
+                form.SetIcon("Resources\\mid.ico");
             }
             else if (Batterylife >= 25 && Batterylife <= 49)
             {
                 _notifyIcon.Icon = new Icon("Resources\\middle.ico");
-                if(notified == true)
+                form.SetIcon("Resources\\middle.ico");
+                if (notified == true)
                 {
                     notified = false;
                 }
@@ -125,6 +154,8 @@ namespace BatteryIcon
             else if (Batterylife >= 16 && Batterylife <= 24)
             {
                 _notifyIcon.Icon = new Icon("Resources\\low.ico");
+                form.SetIcon("Resources\\low.ico");
+
                 if (notified == false && (Batterylife >= 16 && Batterylife <= 20))
                 {
                      new ToastContentBuilder()
@@ -139,17 +170,20 @@ namespace BatteryIcon
             else if (Batterylife >= 15)
             {
                 _notifyIcon.Icon = new Icon("Resources\\verylow.ico");
+                form.SetIcon("Resources\\verylow.ico");
             }
         }
 
-        public static void SetText(Forms.PowerStatus _pwr)
+        public void SetText(Forms.PowerStatus _pwr)
         {
             Double BatteryLifeRemaining = Math.Round(_pwr.BatteryLifeRemaining / 60.0);
             Double Batterylife = Math.Round(_pwr.BatteryLifePercent * 100);
+            form.SetPercent(Batterylife.ToString() + "%");
 
             if (_pwr.PowerLineStatus == Forms.PowerLineStatus.Online)
             {
                 _notifyIcon.Text = Batterylife.ToString() + "% available (plugged in)";
+                form.SetTimeRemaining(Batterylife.ToString() + "% available (plugged in)");
             }
             else
             {
@@ -158,10 +192,12 @@ namespace BatteryIcon
                 if (hrs == 0)
                 {
                     _notifyIcon.Text = Batterylife.ToString() + "%" + " remaining";
+                    form.SetTimeRemaining(Batterylife.ToString() + "%" + " remaining");
                 }
                 else
                 {
                     _notifyIcon.Text = $"{hrs} hr {mins} min " + " " + "(" + Batterylife.ToString() + "%" + ") remaining";
+                    form.SetTimeRemaining($"{hrs} hr {mins} min " + " " + "(" + Batterylife.ToString() + "%" + ") remaining");
                 }
             }
         }
@@ -181,6 +217,7 @@ namespace BatteryIcon
         private void AggregateBattery_ReportUpdated(Battery sender, object args)
         {
             //make async
+
             SetIcon(_pwr);
             SetText(_pwr);
 
--- BatteryIcon/MainWindow.xaml.cs
+++ BatteryIcon/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using System.Windows;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace BatteryIcon
 {
@@ -7,6 +13,7 @@ namespace BatteryIcon
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
         public MainWindow()
         {
             InitializeComponent();
@@ -15,6 +22,43 @@ namespace BatteryIcon
         private void Hyperlink_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("ms-settings:powersleep");
+        }
+
+        public void SetIcon(string s)
+        {
+            BatteryIcon.Dispatcher.Invoke(() =>
+            {
+                BatteryIcon.Source = new BitmapImage(new Uri(@s, UriKind.Relative));
+            });
+        }
+        public void SetPercent(string s)
+        {
+            Percent.Dispatcher.Invoke(() =>
+            {
+                Percent.Text = s;
+            });
+        }
+
+        public void SetTimeRemaining(string s)
+        {
+            TimeRemaining.Dispatcher.Invoke(() =>
+            { 
+                TimeRemaining.Text = s;
+            });
+        }
+        public TextBlock SetSliderDescription()
+        {
+            return this.FindName("SliderDescription") as TextBlock;
+        }
+
+        public Slider SetPowerSlider()
+        {
+            return this.FindName("PowerSlider") as Slider;
+        }
+
+        public Slider SetBrightnessSlider()
+        {
+            return this.FindName("BrightnessSlider") as Slider;
         }
     }
 }
